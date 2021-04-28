2725f532986f09a36b022658f3c3e19492bb1a62
Sander Nicolaysen <sander@Sanders-MacBook-Air.local>
2021-04-28 20:53:04 +0200

    added basic setup and header

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,17 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+    return (
+        <section className={classes.summary}>
+            <h2>Delious Food, Delivered To You</h2>
+            <p>
+                Et laboris laborum nisi ad quis. Eiusmod cupidatat quis non ullamco occaecat nisi sit nulla voluptate deserunt exercitation. Non sunt commodo pariatur elit in minim officia. Sit sint consectetur ad et.
+            </p>
+            <p>
+                Cupidatat quis occaecat consequat consequat eiusmod dolore adipisicing irure laborum. Exercitation consectetur exercitation anim id aliquip esse et. Minim pariatur magna duis aliqua pariatur deserunt qui incididunt qui et minim elit tempor.
+            </p>
+        </section>
+    );
+}
+
+export default MealsSummary;
